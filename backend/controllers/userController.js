8a295df41fd9e09d1952d56982133d596da1a5df
urizennnn <igamerryt@gmail.com>
2024-05-02 18:25:42 +0100

    fix(core): fix register function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,63 +2,56 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
-
-
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
 
 const registerUser = asyncHandler(async (req, res) => {
-    const { name, email, password } = req.body
+    const { name, email, password } = req.body;
 
-    //validation
+    // Validation
     if (!name || !email || !password) {
-        res.status(400)
-        throw new Error("Please fill all required fields")
+        res.status(400);
+        throw new Error("Please fill all required fields");
     }
     if (password.length < 6) {
-        res.status(400)
-        throw new Error("Password must be up to 8 characters")
+        res.status(400);
+        throw new Error("Password must be at least 6 characters");
     }
-    //check if user exists
+
+    // Check if user exists
     let userExists;
     try {
-        userExists = await User.findOne({ email }).maxTimeMS(20000);
-        // Handle userExist
-    } catch (error) {
-        if (error instanceof Error) {
-            console.error('Mongoose error:', error);
-            // Handle Mongoose error
-        } else {
-            console.error('Other error:', error);
-            // Handle other errors
+        userExists = await User.findOne({ email });
+        if (userExists) {
+            res.status(400);
+            throw new Error("Email has already been registered");
         }
+    } catch (error) {
+        console.error('Mongoose error:', error);
+        res.status(500);
+        throw new Error("Internal Server Error");
     }
 
-
-    if (userExists) {
-        res.status(400)
-        throw new Error("Email has already been registered")
-    }
-
-    //create new user
+    // Create new user
     const user = await User.create({
         name,
         email,
         password,
-    })
+    });
 
-    //generate token
-    const token = generateToken(_id)
+    // Generate token
+    const token = generateToken(user._id);
 
+    // Respond with user data and token
     if (user) {
-        const { _id, name, email } = User
+        const { _id, name, email } = user;
         res.status(201).json({
             _id, name, email, token,
-        })
+        });
     } else {
-        res.status(400)
-        throw new Error("Invalid user data")
+        res.status(400);
+        throw new Error("Invalid user data");
     }
 });
 
--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,19 @@
 const express = require('express');
+const logger = require("morgan")
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const UserRoute = require("./routes/userRoute");
 const errorHandler = require("./middleWare/errorMiddleWare");
-
+const cors = require("cors")
 dotenv.config({ path: './config.env' });
-const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri = process.env.MONGO_URI;
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
-async function run() {
-    try {
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        await client.close();
-    }
-}
-run().catch(console.dir);
-
 const app = express();
 
 //middlewares
 app.use(express.json())
-
+app.use(cors())
+app.use(logger('dev'))
 //error middleware
 app.use(errorHandler);
 
@@ -46,5 +30,9 @@ app.get('/', (_, res) => {
 const PORT = process.env.BACKEND_PORT || 5000;
 
 app.listen(PORT, () => {
+    mongoose.connect(uri).then(() => {
+        console.log("Database connected")
+    })
+
     console.log(`Server is running on port ${PORT}`);
 });
